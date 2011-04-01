b7532e94549585c9fd22266d22a22afbb2cba975
bigbear3001 <bigbear.ap@gmail.com>
2011-04-01 11:54:34 +0200

    first draft of a writable file contentrepository

---
+++ projects/com.dc2f.datastore/src/com/dc2f/datastore/impl/filejson/ChangableJsonNode.java
@@ -0,0 +1,23 @@
+package com.dc2f.datastore.impl.filejson;
+
+import org.json.JSONObject;
+
+import com.dc2f.datastore.ChangableNode;
+import com.dc2f.datastore.ContentRepository;
+import com.dc2f.datastore.NodeType;
+
+public class ChangableJsonNode extends SimpleJsonNode implements ChangableNode {
+
+	public ChangableJsonNode(ContentRepository repository, String path,
+			JSONObject jsonObject, NodeType nodeType) {
+		super(repository, path, jsonObject, nodeType);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setProperty(String propertyName, Object propertyValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
