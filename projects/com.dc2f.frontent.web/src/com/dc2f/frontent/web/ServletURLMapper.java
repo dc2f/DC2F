fedee3b8c69d36a4d734471d30b77c07bc5c6f64
bigbear3001 <bigbear.ap@gmail.com>
2011-04-16 16:29:16 +0200

    get node path from url

--- projects/com.dc2f.frontent.web/src/com/dc2f/frontent/web/ServletURLMapper.java
+++ projects/com.dc2f.frontent.web/src/com/dc2f/frontent/web/ServletURLMapper.java
@@ -1,6 +1,7 @@
 package com.dc2f.frontent.web;
 
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 import com.dc2f.datastore.ContentRepository;
 import com.dc2f.datastore.Node;
@@ -10,12 +11,27 @@ public class ServletURLMapper implements URLMapper {
 
 	private ContentRepository contentRepository;
 	
+	private String defaultProjectPath = "/cmsblog/";
+	
 	public ServletURLMapper(ContentRepository cr) {
 		contentRepository = cr;
 	}
-
+	/**
+	 * Try to load a node for the given servlet request.
+	 * @param request - request to get the node from.
+	 * @return the node for the request if it can be found.
+	 */
 	public Node getNode(ServletRequest request) {
-		return contentRepository.getNode("/cmsblog/articles/my-first-article");
+		if(request instanceof HttpServletRequest) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			String pathFromServlet = httpRequest.getRequestURI().toString();
+			String webappPath = httpRequest.getContextPath();
+			if (pathFromServlet.startsWith(webappPath)) {
+				pathFromServlet = pathFromServlet.substring(webappPath.length());
+			}
+			return contentRepository.getNode(defaultProjectPath + pathFromServlet);
+		}
+		return null;
 	}
 
 	@Override
