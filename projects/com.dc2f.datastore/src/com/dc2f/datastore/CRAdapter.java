421aa210f8e4465f3bcba86960c232a4bc1b2332
Herbert Poul <herbert@poul.at>
2011-04-16 16:52:56 +0200

    added a provider API - e.g. for an xpath search.

---
+++ projects/com.dc2f.datastore/src/com/dc2f/datastore/CRAdapter.java
@@ -0,0 +1,10 @@
+package com.dc2f.datastore;
+
+/**
+ * Adapters provide a generic way of adding additional features to content repositories.
+ * 
+ * @author herbert
+ */
+public interface CRAdapter {
+
+}
