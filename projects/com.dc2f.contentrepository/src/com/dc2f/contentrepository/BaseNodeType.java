dc89ad2c3e024f7bdf713e532e9f16b98b9abd11
bigbear3001 <bigbear.ap@gmail.com>
2011-07-04 22:55:46 +0200

    prevent from NullPointerExceptions when no AttributeDefinition was found

--- projects/com.dc2f.contentrepository/src/com/dc2f/contentrepository/BaseNodeType.java
+++ projects/com.dc2f.contentrepository/src/com/dc2f/contentrepository/BaseNodeType.java
@@ -23,54 +23,56 @@ public abstract class BaseNodeType implements NodeType {
 	public AttributesDefinition getAttributeDefinitions() {
 		if (nodeTypeInfo == null) {
 			logger.severe("node type was not initialized! {" + this.getClass().getName() + "} nodeTypeInfo: {" + nodeTypeInfo + "}");
-		}
-		final Node attrDefinitions = (Node) nodeTypeInfo.get("attributes");
-		final Boolean freeattributes = (Boolean) nodeTypeInfo.get("freeattributes");
-		final String valueType = (String)nodeTypeInfo.get("valuetype");
-		final String valueNodeType = (String) nodeTypeInfo.get("valuenodetype");
-		
-		
-		if (attrDefinitions == null && !valueType.equals("Node")) {
-			logger.severe("Node has no attribute defintions {" + this
-					+ "} (class:" + this.getClass().getName() + "} nodeTypeInfo: {" + nodeTypeInfo + "}");
-		}
-		
-		AttributesDefinition tmpParentAttrDefinitions = null;
-		if (nodeTypeInfo.getParentNodeType() != null) {
-			NodeType parent = nodeTypeInfo.getParentNodeType();
-			tmpParentAttrDefinitions = parent.getAttributeDefinitions();
-		}
-		final AttributesDefinition parentAttrDefinitions = tmpParentAttrDefinitions;
-
-
-		return new AttributesDefinition() {
-			
-			@Override
-			public String[] getAttributeNames() {
-				return nodeTypeInfo.getAttributeNames();
+			return null;
+		} else {
+			final Node attrDefinitions = (Node) nodeTypeInfo.get("attributes");
+			final Boolean freeattributes = (Boolean) nodeTypeInfo.get("freeattributes");
+			final String valueType = (String)nodeTypeInfo.get("valuetype");
+			final String valueNodeType = (String) nodeTypeInfo.get("valuenodetype");
+			if (attrDefinitions == null && !valueType.equals("Node")) {
+				logger.severe("Node has no attribute defintions {" + this
+						+ "} (class:" + this.getClass().getName() + "} nodeTypeInfo: {" + nodeTypeInfo + "}");
 			}
+
 			
-			@Override
-			public Node getAttributeDefinition(String propertyName) {
-					if (freeattributes != null && freeattributes.booleanValue()) {
-						return new MapNode(new KeyValuePair("type", valueType), new KeyValuePair("typeofnode", valueNodeType));
-					}
-					Object def = null;
-					if (attrDefinitions != null) {
-						def = attrDefinitions.get(propertyName);
-					}
-					if (def == null && parentAttrDefinitions != null) {
-						return (Node) parentAttrDefinitions.getAttributeDefinition(propertyName);
-					}
-					return (Node) def;
-				}
-				
+			AttributesDefinition tmpParentAttrDefinitions = null;
+			if (nodeTypeInfo.getParentNodeType() != null) {
+				NodeType parent = nodeTypeInfo.getParentNodeType();
+				tmpParentAttrDefinitions = parent.getAttributeDefinitions();
+			}
+			final AttributesDefinition parentAttrDefinitions = tmpParentAttrDefinitions;
+
+
+			return new AttributesDefinition() {
 				
 				@Override
-				public String toString() {
-					return "{(" + BaseNodeType.this + ")" + String.valueOf(attrDefinitions) + " parent:(" + nodeTypeInfo.getParentNodeType() + ")" + String.valueOf(parentAttrDefinitions) + "}";
+				public String[] getAttributeNames() {
+					return nodeTypeInfo.getAttributeNames();
 				}
-			};
+				
+				@Override
+				public Node getAttributeDefinition(String propertyName) {
+						if (freeattributes != null && freeattributes.booleanValue()) {
+							return new MapNode(new KeyValuePair("type", valueType), new KeyValuePair("typeofnode", valueNodeType));
+						}
+						Object def = null;
+						if (attrDefinitions != null) {
+							def = attrDefinitions.get(propertyName);
+						}
+						if (def == null && parentAttrDefinitions != null) {
+							return (Node) parentAttrDefinitions.getAttributeDefinition(propertyName);
+						}
+						return (Node) def;
+					}
+					
+					
+					@Override
+					public String toString() {
+						return "{(" + BaseNodeType.this + ")" + String.valueOf(attrDefinitions) + " parent:(" + nodeTypeInfo.getParentNodeType() + ")" + String.valueOf(parentAttrDefinitions) + "}";
+					}
+				};
+		}
+		
 	}
 	
 	@Override
