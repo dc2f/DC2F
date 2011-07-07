6194d6d761fba9b03c96864af3bffc6b1f4783c8
Herbert Poul <herbert@poul.at>
2011-07-07 19:41:53 +0200

    undeleted dc2f servlet.

---
+++ projects/com.dc2f.frontent.web/src/com/dc2f/frontent/web/DC2FServlet.java
@@ -0,0 +1,108 @@
+package com.dc2f.frontent.web;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.util.Collections;
+import java.util.logging.Logger;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import com.dc2f.contentrepository.BranchAccess;
+import com.dc2f.contentrepository.CRSession;
+import com.dc2f.contentrepository.ContentRepository;
+import com.dc2f.contentrepository.ContentRepositoryFactory;
+import com.dc2f.contentrepository.Node;
+import com.dc2f.contentrepository.exception.DC2FRuntimeException;
+import com.dc2f.renderer.ContentRenderResponse;
+import com.dc2f.renderer.NodeRenderer;
+import com.dc2f.renderer.NodeRendererFactory;
+import com.dc2f.renderer.impl.ContentRenderRequestImpl;
+
+public class DC2FServlet implements Servlet {
+
+	private ServletConfig config;
+	
+	private static final Logger logger = Logger.getLogger(DC2FServlet.class.getName());
+	
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		return config;
+	}
+
+	@Override
+	public String getServletInfo() {
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig configuration) throws ServletException {
+		config = configuration;
+
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response)
+			throws ServletException, IOException {
+		NodeRendererFactory factory = NodeRendererFactory.getInstance();
+		NodeRenderer renderer = factory.getRenderer("com.dc2f.renderer.web");
+		File crdir = new File(System.getProperty("crdir", "../../design/example"));
+		if (crdir == null || !crdir.exists()) {
+			System.out.println("Please specify a crdir :) ( -Dcrdir=xxx)");
+		}
+		ContentRepository cr = ContentRepositoryFactory.getInstance().getContentRepository("simplejsonfile", Collections.singletonMap("directory", (Object)crdir.getAbsolutePath()));
+		CRSession conn = cr.authenticate(null);
+		BranchAccess craccess = conn.openTransaction(null);
+		//ContentRepository cr = new SimpleFileContentRepository(crdir);
+		ServletURLMapper mapper = new ServletURLMapper(craccess, getServletConfig());
+		Node node = mapper.getNode(request);
+		logger.info("We got a node: {" + node + "}");
+		
+		renderer.renderNode(new ContentRenderRequestImpl(cr, craccess, craccess.getNodesInPath(node.getPath()), mapper),
+					new ServletRenderResponse(response));
+	}
+	
+	private class ServletRenderResponse implements ContentRenderResponse {
+
+		private ServletResponse response;
+
+		public ServletRenderResponse(ServletResponse response) {
+			this.response = response;
+		}
+
+		@Override
+		public OutputStream getOutputStream() {
+			try {
+				return response.getOutputStream();
+			} catch (IOException e) {
+				throw new DC2FRuntimeException("Error while getting output stream", e);
+			}
+		}
+
+		@Override
+		public Writer getWriter() {
+			try {
+				return response.getWriter();
+			} catch (IOException e) {
+				throw new DC2FRuntimeException("Error while getting writer", e);
+			}
+		}
+
+		@Override
+		public void setMimeType(String mimeType) {
+			response.setContentType(mimeType);
+		}
+		
+	}
+
+}
---
+++ projects/com.dc2f.frontent.web/src/com/dc2f/frontent/web/ServletURLMapper.java
@@ -0,0 +1,58 @@
+package com.dc2f.frontent.web;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import com.dc2f.contentrepository.CRAccess;
+import com.dc2f.contentrepository.Node;
+import com.dc2f.renderer.url.URLMapper;
+
+public class ServletURLMapper implements URLMapper {
+
+	private ServletConfig servletConfig;
+	
+	private String defaultProjectPath = "/cmsblog";
+
+	private CRAccess crAccess;
+	
+	public ServletURLMapper(CRAccess crAccess, ServletConfig config) {
+		this.crAccess = crAccess;
+		servletConfig = config;
+	}
+	/**
+	 * Try to load a node for the given servlet request.
+	 * @param request - request to get the node from.
+	 * @return the node for the request if it can be found.
+	 */
+	public Node getNode(ServletRequest request) {
+		if(request instanceof HttpServletRequest) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			String pathFromServlet = httpRequest.getRequestURI().toString();
+			String webappPath = httpRequest.getContextPath();
+			if (pathFromServlet.startsWith(webappPath)) {
+				pathFromServlet = pathFromServlet.substring(webappPath.length());
+			}
+			if ("/".equals(pathFromServlet)) {
+				return crAccess.getNode(defaultProjectPath);
+			}
+			return crAccess.getNode(defaultProjectPath + pathFromServlet);
+		}
+		return null;
+	}
+
+	@Override
+	public String getRenderURL(Node node) {
+		String servletURL = servletConfig.getServletContext().getContextPath();
+		String nodePath = node.getPath();
+		if (nodePath.startsWith(defaultProjectPath)) {
+			nodePath = nodePath.substring(defaultProjectPath.length());
+		}
+		return servletURL + nodePath;
+	}
+	
+	private String beautifyURL(String url) {
+		return url.replaceAll("/{2,}", "/");
+	}
+
+}
