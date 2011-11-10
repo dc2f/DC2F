f301e67e5bb17325fadac7ee4a57600fa70940b1
Herbert Poul <herbert@poul.at>
2011-11-10 15:29:58 +0100

    added rendering of dates.

--- projects/com.dc2f.nodetype/src/com/dc2f/nodetype/Date.java
+++ projects/com.dc2f.nodetype/src/com/dc2f/nodetype/Date.java
@@ -8,26 +8,31 @@ import com.dc2f.contentrepository.BaseNodeType;
 import com.dc2f.contentrepository.Node;
 
 public class Date extends BaseNodeType {
-	
+
 	public Calendar getCalendar(Node node) {
 		TimeZone timeZone = TimeZone.getDefault();
-		
+
 		Integer offset = (Integer) node.get("offset");
 		String[] timeZones = TimeZone.getAvailableIDs(offset);
 		if (timeZones.length > 0) {
 			TimeZone.getTimeZone(timeZones[0]);
 		}
-		
+
 		Long milliseconds = (Long) node.get("timestamp");
 		GregorianCalendar calendar = new GregorianCalendar(timeZone);
 		calendar.setTimeInMillis(milliseconds);
-		
+
 		return calendar;
 	}
-	
+
 	public java.util.Date getDate(Node node) {
-		return getCalendar(node).getTime();
+		return new java.util.Date(((Integer)node.get("timestamp")).intValue() * 1000L);
 	}
 
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString() + " xyz";
+	}
 
 }
