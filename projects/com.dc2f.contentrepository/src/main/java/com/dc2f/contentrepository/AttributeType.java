d7bcf611c87fbf97d84f1d5ce46390536b7f26d8
Andreas Perhab <bigbear.ap@gmail.com>
2012-12-22 18:10:17 +0100

    added some javadoc

--- projects/com.dc2f.contentrepository/src/main/java/com/dc2f/contentrepository/AttributeType.java
+++ projects/com.dc2f.contentrepository/src/main/java/com/dc2f/contentrepository/AttributeType.java
@@ -3,12 +3,30 @@ package com.dc2f.contentrepository;
 import com.dc2f.contentrepository.exception.InvalidAttributeTypeException;
 
 public enum AttributeType {
+	/**
+	 * Attribute type for attributes that contain boolean values (true, false).
+	 */
 	BOOLEAN("Boolean"),
+	/**
+	 * Attribute type for attributes that contain strings.
+	 */
 	STRING("String"),
+	/**
+	 * Attribute type for attributes that contain integers.
+	 */
 	INTEGER("Integer"),
+	/**
+	 * Attribute type for attributes that contain another node. This is used for links.
+	 */
 	NODE_REFERENCE("NodeReference"),
 	NODETYPE_REFERENCE("NodeTypeReference"),
+	/**
+	 * Attribute type for attributes that contain large texts.
+	 */
 	CLOB("clob"),
+	/**
+	 * Attribute type for attributes that contain binaries (images and documents).
+	 */
 	BLOB("blob"),
 	NODE("Node"),
 	LIST_OF_NODES("ListOfNodes")
