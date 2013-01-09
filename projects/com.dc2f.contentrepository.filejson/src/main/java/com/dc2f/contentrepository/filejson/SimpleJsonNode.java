368354163f87f7b5a5b416ccae7e9d90df11fe2e
Andreas Perhab <bigbear.ap@gmail.com>
2013-01-09 16:01:36 +0100

    replaced new Integer with valueof

--- projects/com.dc2f.contentrepository.filejson/src/main/java/com/dc2f/contentrepository/filejson/SimpleJsonNode.java
+++ projects/com.dc2f.contentrepository.filejson/src/main/java/com/dc2f/contentrepository/filejson/SimpleJsonNode.java
@@ -178,7 +178,7 @@ public class SimpleJsonNode implements Node {
 			if (obj instanceof Integer) {
 				return obj;
 			}
-			return new Integer(obj.toString());
+			return Integer.valueOf(obj.toString());
 		}
 		logger.info("getting property " + attributeName + " --- attrDefinitions: " + attrDefinitions + " attrDefinition: " + attrDefinition);
 		logger.severe("FIXME: Not Implemented: Unable to convert property {" + attributeName + "} of node type {" + getName() + "}: {" + obj.getClass().getName() + "} - attributeType: {" + attributeType + "}");
