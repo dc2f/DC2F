938b1a912cdbe4fa9019685a7cebe92eab84b027
Herbert Poul <herbert@poul.at>
2013-01-10 17:00:03 +0100

    checkstyle fixes.

--- projects/com.dc2f.backend.gwt/src/main/java/com/dc2f/backend/gwt/shared/DTOAttributesDefinition.java
+++ projects/com.dc2f.backend.gwt/src/main/java/com/dc2f/backend/gwt/shared/DTOAttributesDefinition.java
@@ -1,33 +1,63 @@
 package com.dc2f.backend.gwt.shared;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Attribute definitions representation as DTO.
+ */
 public class DTOAttributesDefinition implements Serializable {
+	/** **/
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Attribute names available for this node.
+	 */
 	private String[] attributeNames;
+	
+	/**
+	 * all attribute definitions for each attribute.
+	 */
 	private Map<String, Node> attributeDefinition = new HashMap<String, Node>();
 	
+	/**
+	 * Required empty constructor.
+	 */
 	public DTOAttributesDefinition() {
 	}
 	
-	public DTOAttributesDefinition(String[] attributeNames) {
+	/**
+	 * creates a new attribute definition with the given attribute names.
+	 * @param attributeNames available attribute names
+	 */
+	public DTOAttributesDefinition(final String[] attributeNames) {
 		this.attributeNames = attributeNames;
 	}
 
-	public Node getAttributeDefinition(String propertyName) {
+	/**
+	 * not yet implemented.
+	 * @param propertyName attribute name
+	 * @return null
+	 */
+	public Node getAttributeDefinition(final String propertyName) {
 		return null;
 	}
 	
+	/**
+	 * @return all available attribute names.
+	 */
 	public String[] getAttributeNames() {
 		return attributeNames;
 	}
 
-	public void setAttributeDefinition(String attributeName,
-			ContentNode dtoNode) {
+	/**
+	 * 
+	 * @param attributeName attribute name
+	 * @param dtoNode attribute definition node
+	 */
+	public void setAttributeDefinition(final String attributeName,
+			final ContentNode dtoNode) {
 		attributeDefinition.put(attributeName, dtoNode);
 	}
 
