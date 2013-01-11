bfffe367d212830181d4fe05917f1eb75d752f7e
Herbert Poul <herbert@poul.at>
2013-01-11 15:45:43 +0100

    formatting, a bit more checkstyling.

--- projects/com.dc2f.contentrepository/src/main/java/com/dc2f/contentrepository/Node.java
+++ projects/com.dc2f.contentrepository/src/main/java/com/dc2f/contentrepository/Node.java
@@ -2,31 +2,37 @@ package com.dc2f.contentrepository;
 
 /**
  * A Node is the base data container for DC2F. It has a name and a unique path.
+ * 
  * @author herbert
  */
 public interface Node {
 	/**
 	 * The name of the node which is only unique within the parent of this node.
+	 * 
+	 * @return returns the base name of this node.
 	 */
 	String getName();
-	
+
 	/**
 	 * The type of this node.
+	 * 
+	 * @return node type descriptor of this node.
 	 */
 	NodeType getNodeType();
 
 	/**
-	 * the unique path to this node (contains the name) - returned normalized (e.g.
-	 * /cmsblog/articles/my-first-article) - Starting with a slash, no tailing
-	 * slash!
+	 * the unique path to this node (contains the name) - returned normalized (e.g. /cmsblog/articles/my-first-article) - Starting with a slash, no tailing slash!
+	 * 
+	 * @return content repository path to this node.
 	 */
 	String getPath();
-	
-	
-	
+
 	/**
 	 * returns an attribute for this node.
+	 * 
+	 * @param attributeName name of an attribute to be returned
+	 * @return value of that attribute for this node.
 	 */
 	Object get(String attributeName);
-	
+
 }
--- projects/com.dc2f.contentrepository/src/main/java/com/dc2f/contentrepository/nodetypedefinition/NodeTypeDefinition.java
+++ projects/com.dc2f.contentrepository/src/main/java/com/dc2f/contentrepository/nodetypedefinition/NodeTypeDefinition.java
@@ -3,6 +3,12 @@ package com.dc2f.contentrepository.nodetypedefinition;
 import com.dc2f.contentrepository.AttributesDefinition;
 import com.dc2f.contentrepository.BaseNodeType;
 
+/**
+ * a static NodeType which represents an actual NodeType definition. (Since we can't define the
+ * structure of a NodeType - Node using a Node.. we have to hardcode it..)
+ * 
+ * @author herbert
+ */
 public class NodeTypeDefinition extends BaseNodeType {
 
 	@Override
@@ -10,15 +16,16 @@ public class NodeTypeDefinition extends BaseNodeType {
 		return new MapAttributeDefinition(
 				new KeyValuePair("_comment", new MapNode(new KeyValuePair("type", "String"))),
 				new KeyValuePair(
-						"attributes", new MapNode(
+						"attributes",
+						new MapNode(
 								new KeyValuePair("type", "Node"),
-								new KeyValuePair("typeofnode", "classpath:com/dc2f/contentrepository/nodetypedefinition/AttributesNodeType")
-								/*,
-								new KeyValuePair("typeofnode", "classpath:com/dc2f/contentrepository/DefaultNodeType")*/) ),
+								new KeyValuePair("typeofnode",
+										"classpath:com/dc2f/contentrepository/nodetypedefinition/AttributesNodeType")
+						)),
 				new KeyValuePair("class", new MapNode(new KeyValuePair("type", "String"))),
 				new KeyValuePair("freeattributes", new MapNode(new KeyValuePair("type", "Boolean"))),
 				new KeyValuePair("valuetype", new MapNode(new KeyValuePair("type", "String"))),
 				new KeyValuePair("valuenodetype", new MapNode(new KeyValuePair("type", "String"))));
 	}
-	
+
 }
